4df88e721223ca45b4ab5fbc5a6f8565df464ef8
8ward <1367048+8ware@users.noreply.github.com>
2022-07-27 23:27:23 +0200

    Provide screen by index, fallback to hidden shelf
    This allows to define multiple taglists each starting on a specific screen.
    If there are not enough screens available the taglists will initially be
    managed by the shelf screen.

--- init.lua
+++ init.lua
@@ -1,5 +1,19 @@
 
+local capi = {
+    screen = screen,
+}
+
+
 local versatile = {}
+
+local shelf = capi.screen.fake_add(
+    -capi.screen[1].geometry.width, -capi.screen[1].geometry.height,
+     capi.screen[1].geometry.width,  capi.screen[1].geometry.height
+)
+
+function versatile.screen(index)
+    return capi.screen[index] or shelf
+end
 
 return versatile
 
